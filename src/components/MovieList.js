227c672b91f3703643c13e1de41c1499e2096377
BatoolBtoush <batoolbtoush98@gmail.com>
2022-02-16 04:20:41 +0200

    not done task18

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,29 +1,56 @@
 import React from "react";
 //import Movie from "./Movie";
 import { Card, CardGroup } from "react-bootstrap";
+// import ModalMovie from "./ModalMovie";
 
-function MovieList(props) {
-  // looping through the fetched data
-   console.log(props);
 
-   return (
-    <>
-       <CardGroup>
-         {props.trending.map((trend) => {
-        return (
-            <div>
-              <Card>
-                <Card.Body>
-                  <Card.Title>{trend.title}</Card.Title>
-                  <Card.Text></Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-          );
-        })}
-      </CardGroup> 
-   </>
-  );
+
+function MoviesList(props) {
+  // const [showModal, setShowModal] = useState(false);
+  return (
+      <>
+          <CardGroup style={{ display: "flex", justifyContent: "space-around" }}>
+              {
+                  props.trending.map(trend => {
+                      return <div key={trend.id}>
+                          <Card key={trend.id} >
+                              <Card.Body>
+                                  <Card.Title>{trend.title}</Card.Title>
+                              </Card.Body>
+                          </Card>
+                      </div>
+                  })
+              }
+          </CardGroup>
+          {/* {showModal && <ModalMovie show={showModal} meme={meme} handleColse={() => { setShowModal(false) }} updateCaption={props.updateCaption} />} */}
+      </>
+  )
 }
 
-export default MovieList;
+export default MoviesList;
+
+// function MovieList(props) {
+//   // looping through the fetched data
+
+
+//    return (
+//     <>
+//        <CardGroup>
+//          {props.trending.map((trend) => {
+//         return (
+//             <div>
+//               <Card>
+//                 <Card.Body>
+//                   <Card.Title>{trend.title}</Card.Title>
+//                   <Card.Text></Card.Text>
+//                 </Card.Body>
+//               </Card>
+//             </div>
+//           );
+//         })}
+//       </CardGroup> 
+//    </>
+//   );
+// }
+
+// export default MovieList;
